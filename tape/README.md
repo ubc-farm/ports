09f16e7a5e305112f8dadd8dd7d56d5071d1ac5b
Tiger Oakes <tigeroakes@gmail.com>
2016-08-23 10:18:53 -0700

    Moved JSTS to root for JSTS branch

--- tape/README.md
+++
@@ -1,2 +0,0 @@
-# tape
-A copy of the [tape](https://github.com/substack/tape) library, compiled for browsers using browserify. 
--- vis-timeline/index.js
+++
@@ -1,3 +0,0 @@
-//export {default} from './react-timeline.js';
-export {default} from './src/timeline/Timeline.js';
-export {default as DataSet} from './src/DataSet.js';
